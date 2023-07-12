e49a8938d8efc95316590de7152df4180c409c4a
jmontmaxwell <82671719+jmontmaxwell@users.noreply.github.com>
2023-07-12 16:04:29 -0500

    Revert "update files"

--- TCGA_STAD_Nextflow/mixed_datatype_clustering.py
+++ TCGA_STAD_Nextflow/mixed_datatype_clustering.py
@@ -28,15 +28,14 @@ maf_df = pd.read_csv(maf_file)
 X_full = maf_df.values
 X = X_full
 
-N = X.shape[0]
-P = X.shape[1]
+P = X.shape[0]
+N = X.shape[1]
 
 # trials = len(clusters) * len(distance_types) * len(hyperparams)
 
 Scores = np.zeros((1, 1))
-all_feature_weights = np.zeros((P, 1))
-all_feature_orders = np.zeros((P, 1))
-all_cluster_labels = np.zeros((N, 1))
+all_feature_weights = np.zeros((N, 1))
+all_cluster_labels = np.zeros((P, 1))
 
 result_labels = []
 
@@ -47,15 +46,15 @@ for K in clusters:
 
         for S in hyperparams:
 
-            results_path_prefix = f"P={P}_K={K}_dist={distance}_S={S}"
+            results_path_prefix = f"N={N}_K={K}_dist={distance}_S={S}"
             result_labels.append(results_path_prefix)
 
-            print(f"P={P} | K={K} | {distance} {S} started")
+            print(f"N={N} | K={K} | {distance} {S} started")
 
             (
                 cluster_labels,
                 feature_weights,
-                feature_order,
+                feature_order1,
                 weighted_distances,
             ) = clustering.sparse_kmedoids(
                 X,
@@ -75,12 +74,6 @@ for K in clusters:
 
             all_feature_weights[:, iter] = feature_weights
             all_cluster_labels[:, iter] = cluster_labels
-            feature_order_vec = (
-                feature_order["Numerical Features"]
-                + feature_order["Binary Features"]
-                + feature_order["Categorical Features"]
-            )
-            all_feature_orders[:, iter] = feature_order_vec
 
             weighted_distances_df = pd.DataFrame(weighted_distances)
             os.makedirs(
@@ -117,10 +110,3 @@ os.makedirs(
     exist_ok=True,
 )
 Scores_df.to_csv("results/" + results_path_prefix + "_cluster_scores.csv", index=False)
-
-orders_df = pd.DataFrame(all_feature_orders, columns=result_labels)
-os.makedirs(
-    os.path.dirname("results/" + results_path_prefix + "_feature_orders.csv"),
-    exist_ok=True,
-)
-orders_df.to_csv("results/" + results_path_prefix + "_feature_orders.csv", index=False)
